b28cc9fbacfc9f28bd006b46d794ed8df251db46
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-12 17:24:20 -0300

    Player status class updated

---
+++ jogo_da_forca/src/main/Player.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Player {
+	
+	private int life;
+	private int score;
+	
+	// Init
+	public Player () {
+		
+	}
+	
+	// Getters and setters
+	public int getLife() {
+		return life;
+	}
+	public void setLife(int life) {
+		this.life = life;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
