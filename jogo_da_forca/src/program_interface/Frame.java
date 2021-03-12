c78a44d5ecab0b2a240823b27d1e97c37ff40118
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-12 17:23:10 -0300

    JFrame interface class created

--- jogo_da_forca/src/program_interface/Frame.java
+++ jogo_da_forca/src/program_interface/Frame.java
@@ -2,25 +2,27 @@ package program_interface;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Frame extends JFrame{
 
-	Frame(String titulo){
-		this.setTitle(titulo); // Title
+	// Init
+	Frame(String title){
+		
+		Button button = new Button("A", 100, 500);
+		
+		this.add(button);
+		
+		this.setTitle(title); // Title
+		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close button shut down the program
 		this.setResizable(false);
-		this.setSize(800,800); // Size of the window
+		this.setSize(1200,800); // Size of the window
 		this.setVisible(true);
 		this.getContentPane().setBackground(Color.white);
-	}
-	
-	Frame(){
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close button shut down the program
-		this.setResizable(false);
-		this.setSize(800,800); // Size of the window
-		this.setVisible(true);
-		this.getContentPane().setBackground(Color.white);
+		
+		
 	}
 	
 }
