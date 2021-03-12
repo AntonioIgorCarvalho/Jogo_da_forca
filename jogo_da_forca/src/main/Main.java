22d32af07544a33cac572448de8f4d0d8c2b0bca
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-12 17:25:01 -0300

    Main class

--- jogo_da_forca/src/main/Main.java
+++ jogo_da_forca/src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
-import program_interface.Interface;;
+import program_interface.Interface;
 
 public class Main {
 	
 	public static void main(String args[]) {
+		
+		Word brasil = new Word("Brasil","De todos os países, é o único que disputou todas as copas do mundo","paises");
+		
+		Interface.main(args);
 		
 	}
 
