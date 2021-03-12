82d4bf3f036d33dc737c980a9a34cb4733c68f22
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-12 17:24:06 -0300

    Interface manager class created

--- jogo_da_forca/src/program_interface/Interface.java
+++ jogo_da_forca/src/program_interface/Interface.java
@@ -1,21 +1,15 @@
 package program_interface;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 public class Interface {
 	
 public static void main(String args[]) {
 	
-	JLabel label = new JLabel();
-	
-	ImageIcon image = new ImageIcon("forca.png");
-	label.setText("Hey hey");
-	label.setIcon(image);
-	
-	
-	Frame frame = new Frame("Menu"); 
-	frame.add(label);
+
+	Frame frame = new Frame("Letters"); 
 		
 	}
 
