6d983b8ec415836da3c081c550842bb3c82e0659
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-14 20:40:46 -0300

    Some new parameters passed

--- jogo_da_forca/src/main/Main.java
+++ jogo_da_forca/src/main/Main.java
@@ -2,15 +2,16 @@ package main;
 import program_interface.Frame;
 
 public class Main {
-	
+
 	public static void main(String args[]) {
-		
+
 		Word brasil = new Word("BRASIL","De todos os países, é o único que disputou todas as copas do mundo","paises");//Word for the game
-		
+
 		Player player = new Player();
 		Game game = new Game(player, brasil);
 		Frame frame = new Frame("Jogo", game);
-		
+		game.setFrame(frame);//Add frame as parameter to game
+
 		Controller controller = new Controller(game, frame);//Set the controller
 		
 		controller.game.setWord(brasil);
