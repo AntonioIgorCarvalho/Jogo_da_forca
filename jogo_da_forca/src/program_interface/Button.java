994cbe0634b506b4b14d80f3e711ca8d1710354d
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-14 14:11:12 -0300

    New button action added

--- jogo_da_forca/src/program_interface/Button.java
+++ jogo_da_forca/src/program_interface/Button.java
@@ -1,17 +1,21 @@
 package program_interface;
+import main.Game;
 
 import java.awt.Color;
 
 import javax.swing.JButton;
 
 public class Button extends JButton {
+	Game game;
 
-	public Button(String title, int x, int y){
+	public Button(String title, int x, int y, Game game){
+	this.game = game;
 	
 	this.setBounds(x, y, 50, 50);
 	this.setText(title);
 	this.setVisible(true);
 	this.setFocusable(false);
+	this.addActionListener(e -> this.game.LetterChoice(title.charAt(0)));//Action when the button is clicked
 
 	}
 }
