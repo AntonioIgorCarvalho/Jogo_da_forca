e9fd7ef169ce741e3f3a395d86d0d59e0b923eb2
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-14 14:11:33 -0300

    Controller class created

---
+++ jogo_da_forca/src/main/Controller.java
@@ -0,0 +1,15 @@
+package main;
+import program_interface.Frame;
+
+public class Controller {
+	
+	Game game;
+	Frame frame;
+	
+	public Controller(Game game, Frame frame) {
+		this.game = game;
+		this.frame = frame;
+	}
+	
+}
+
