6247a22a8bad28b3d95d7db5c35bc021389514b3
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-15 16:51:01 -0300

    Refactoring

--- jogo_da_forca/src/main/Main.java
+++ jogo_da_forca/src/main/Main.java
@@ -5,17 +5,17 @@ public class Main {
 
 	public static void main(String args[]) {
 
-		Word brasil = new Word("BRASIL","De todos os países, é o único que disputou todas as copas do mundo","paises");//Word for the game
+		Word brasil = new Word("BRAASIL","De todos os países, é o único que disputou todas as copas do mundo","paises");//Word for the game
 
 		Player player = new Player();
 		Game game = new Game(player, brasil);
 		Frame frame = new Frame("Jogo");
 		game.setFrame(frame);//Add frame as parameter to game
 
-		Controller controller = new Controller(game, frame);//Set the controller
+		new Controller(game, frame);//Set the controller
 		
-		controller.game.setWord(brasil);
-		controller.game.StartGame();
+		Controller.game.setWord(brasil);
+		Controller.game.StartGame();
 	}
 
 }
