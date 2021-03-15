dc3e5eb3583a09f39a8ba69d96b18ce3127cb771
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-15 00:32:34 -0300

    Refactoring code

--- jogo_da_forca/src/program_interface/Button.java
+++ jogo_da_forca/src/program_interface/Button.java
@@ -1,19 +1,18 @@
 package program_interface;
-import main.Game;
+import main.Controller;
 import javax.swing.JButton;
 
 public class Button extends JButton {
-	Game game;
 
-	public Button(String title, int x, int y, Game game){
-	this.game = game;
+	public Button(Character title, int x, int y){
 	
 	this.setBounds(x, y, 50, 50);
-	this.setText(title);
+	this.setText(title.toString());
 	this.setVisible(true);
 	this.setFocusable(false);
-	this.addActionListener(e -> this.game.letterChoice(title.charAt(0)));//Action when the button is clicked
+	this.addActionListener(e -> Controller.game.letterChoice(title));//Action when the button is clicked
 	this.addActionListener(e -> this.setEnabled(false));//Action when the button is clicked
 	
 	}
-}
+
+	}
--- jogo_da_forca/src/program_interface/Frame.java
+++ jogo_da_forca/src/program_interface/Frame.java
@@ -7,72 +7,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Frame extends JFrame{
-	Game game;
+
 	Label wordShow = new Label(75, 200, 400, 400);
 
 	// Init
-	public Frame(String title, Game game){
-		this.game = game;//Game object in order to pass this for the buttons to use the Game functions
-		
-		int spaceB = 25;
-		int spaceX = 50;
-		int spaceY = 500;
-		int spaceY2 = spaceY  + spaceB + 50;
-		
-		//Creating buttons
-		Button buttonA = new Button("A", (spaceB) + (spaceX), spaceY, game);
-		Button buttonB = new Button("B", (spaceB * 2) + (spaceX * 2), spaceY, game);
-		Button buttonC = new Button("C", (spaceB * 3) + (spaceX * 3), spaceY, game);
-		Button buttonD = new Button("D", (spaceB * 4) + (spaceX * 4), spaceY, game);
-		Button buttonE = new Button("E", (spaceB * 5) + (spaceX * 5), spaceY, game);
-		Button buttonF = new Button("F", (spaceB * 6) + (spaceX * 6), spaceY, game);
-		Button buttonG = new Button("G", (spaceB * 7) + (spaceX * 7), spaceY, game);
-		Button buttonH = new Button("H", (spaceB * 8) + (spaceX * 8), spaceY, game);
-		Button buttonI = new Button("I", (spaceB * 9) + (spaceX * 9), spaceY, game);
-		Button buttonJ = new Button("J", (spaceB * 10) + (spaceX * 10), spaceY, game);
-		Button buttonK = new Button("K", (spaceB * 11) + (spaceX * 11), spaceY, game);
-		Button buttonL = new Button("L", (spaceB * 12) + (spaceX * 12), spaceY, game);
-		Button buttonM = new Button("M", (spaceB * 13) + (spaceX * 13), spaceY, game);
-		Button buttonN = new Button("N", (spaceB) + (spaceX), spaceY2, game);
-		Button buttonO = new Button("O", (spaceB * 2) + (spaceX * 2), spaceY2, game);
-		Button buttonP = new Button("P", (spaceB * 3) + (spaceX * 3), spaceY2, game);
-		Button buttonQ = new Button("Q", (spaceB * 4) + (spaceX * 4), spaceY2, game);
-		Button buttonR = new Button("R", (spaceB * 5) + (spaceX * 5), spaceY2, game);
-		Button buttonS = new Button("S", (spaceB * 6) + (spaceX * 6), spaceY2, game);
-		Button buttonT = new Button("T", (spaceB * 7) + (spaceX * 7), spaceY2, game);
-		Button buttonU = new Button("U", (spaceB * 8) + (spaceX * 8), spaceY2, game);
-		Button buttonV = new Button("V", (spaceB * 9) + (spaceX * 9), spaceY2, game);
-		Button buttonW = new Button("W", (spaceB * 10) + (spaceX * 10), spaceY2, game);
-		Button buttonX = new Button("X", (spaceB * 11) + (spaceX * 11), spaceY2, game);
-		Button buttonY = new Button("Y", (spaceB * 12) + (spaceX * 12), spaceY2, game);
-		Button buttonZ = new Button("Z", (spaceB * 13) + (spaceX * 13), spaceY2, game);
-		
-		this.add(buttonA);
-		this.add(buttonB);
-		this.add(buttonC);
-		this.add(buttonD);
-		this.add(buttonE);
-		this.add(buttonF);
-		this.add(buttonG);
-		this.add(buttonH);
-		this.add(buttonI);
-		this.add(buttonJ);
-		this.add(buttonK);
-		this.add(buttonL);
-		this.add(buttonM);
-		this.add(buttonN);
-		this.add(buttonO);
-		this.add(buttonP);
-		this.add(buttonQ);
-		this.add(buttonR);
-		this.add(buttonS);
-		this.add(buttonT);
-		this.add(buttonU);
-		this.add(buttonV);
-		this.add(buttonW);
-		this.add(buttonX);
-		this.add(buttonY);
-		this.add(buttonZ);
+	public Frame(String title){
+
+		drawButtons();
 		
 		this.setTitle(title); // Title
 		this.setLayout(null);
@@ -86,6 +27,25 @@ public class Frame extends JFrame{
 		this.add(wordShow);
 	}
 	
+	private void drawButtons() {
+		int spaceX = 50;
+		int spaceY = 500;	
+		
+		//Creating letter buttons from A to Z
+		int aux = 1;
+		for(int i = 65; i < 91; i++) {
+
+			Button button = new Button((char) i, (25 + spaceX ) * aux, spaceY);
+			aux++;
+			if(aux == 14) {
+				spaceY += 25 + 50;
+				aux = 1;
+			}
+			this.add(button);
+		}
+		
+	}
+
 	public void setWordShow(String text) {
 		this.wordShow.setText(text);
 		this.wordShow.setVisible(true);
