7614ce3eb04995701cad5bc4cf01216dcdb81fbc
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-03-30 19:26:24 -0300

    Transfer method from Frame to GamePanel

--- jogo_da_forca/src/program_interface/Frame.java
+++ jogo_da_forca/src/program_interface/Frame.java
@@ -83,27 +83,6 @@ public class Frame extends JFrame{
 		this.choice.setVisible(true);
 		SwingUtilities.updateComponentTreeUI(this);//Updating the frame
 	}
-	
-	/*
-	 * Creating button letters from A to Z and adding them on the frame
-	 */
-	protected void drawButtons() {
-		int spaceX = 50;
-		int spaceY = 500;	
-		
-		int aux = 1;
-		for(int i = 65; i < 91; i++) {
-
-			Button button = new Button((char) i, (25 + spaceX ) * aux, spaceY);
-			aux++;
-			if(aux == 14) {
-				spaceY += 25 + 50;
-				aux = 1;
-			}
-			this.game.add(button);
-		}
-		
-	}
 
 	/*
 	 * setter of the wordshow
--- jogo_da_forca/src/program_interface/GamePanel.java
+++ jogo_da_forca/src/program_interface/GamePanel.java
@@ -37,13 +37,34 @@ public class GamePanel extends JPanel {
 		menu.addActionListener(e -> this.frame.setVisibleMenu());
 		this.add(menu);
 		
-		frame.drawButtons();
+		this.drawButtons();
 		
 		//Adding labels
 		this.add(frame.getWordShow());
 		this.add(frame.getLifeShow());
 		
 		this.frame.add(this);//Adding the gameJPanel to the frame
+	}
+	
+	/*
+	 * Creating button letters from A to Z and adding them on the frame
+	 */
+	protected void drawButtons() {
+		int spaceX = 50;
+		int spaceY = 500;	
+		
+		int aux = 1;
+		for(int i = 65; i < 91; i++) {
+
+			Button button = new Button((char) i, (25 + spaceX ) * aux, spaceY);
+			aux++;
+			if(aux == 14) {
+				spaceY += 25 + 50;
+				aux = 1;
+			}
+			this.add(button);
+		}
+		
 	}
 
 }
