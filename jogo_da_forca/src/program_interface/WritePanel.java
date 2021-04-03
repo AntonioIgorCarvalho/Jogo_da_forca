5bc77f4e437ef05983f216ee14a535cc20b39991
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-04-03 20:25:30 -0300

    New class for interface when the user will write a new word

---
+++ jogo_da_forca/src/program_interface/WritePanel.java
@@ -0,0 +1,133 @@
+package program_interface;
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import main.Controller;
+
+public class WritePanel extends JPanel {
+	private Frame frame;
+	private String text;
+	private String tip;
+	private String type;
+	
+	/*
+	 * Configuring WritePanel
+	 */
+	public WritePanel(Frame frame) {
+		this.frame = frame;
+		Color transparent = new Color(0, 0, 0, 0);
+		this.setBounds(0, 0, 1200, 800);
+		this.setBackground(transparent);
+		this.setLayout(null);
+	}
+	
+	/*
+	 * Interface of the WritePanel
+	 */
+	public void updateWritePanel(String type) {
+		this.removeAll();
+		this.type = type;
+		
+		Label instruction = new Label(100, 0, 1200, 100, 40);
+		instruction.setVisible(true);
+		instruction.setText("Use sempre letra maiuscula na palavra. Use '_' para espaco");
+		this.add(instruction);
+		
+		Label instruction2 = new Label(100, 60, 1000, 100, 40);
+		instruction2.setVisible(true);
+		instruction2.setText("Exemplo: KURT_COBAIN");
+		this.add(instruction2);
+		
+		Label textLabel = new Label(100, 130, 1000, 100, 20);
+		textLabel.setVisible(true);
+		textLabel.setText("Insira a nova palavra abaixo: (MAXIMO 16 LETRAS INCLUINDO ESPACO)");
+		this.add(textLabel);
+		
+		JTextField text = new JTextField(16);
+		text.setBounds(100, 200, 120, 40);
+		text.setVisible(true);
+		this.add(text);
+		
+		Label instructionTip = new Label(100, 300, 1200, 100, 40);
+		instructionTip.setVisible(true);
+		instructionTip.setText("A dica pode ser escrita normalmente (Nao use acentos)");
+		this.add(instructionTip);
+		
+		Label instructionTip2 = new Label(100, 360, 1000, 100, 40);
+		instructionTip2.setVisible(true);
+		instructionTip2.setText("Exemplo de dica: Nasceu nos Estados Unidos");
+		this.add(instructionTip2);
+		
+		Label instructionTip3 = new Label(100, 430, 1200, 100, 20);
+		instructionTip3.setVisible(true);
+		instructionTip3.setText("Insira a dica abaixo: (MAXIMO NAO DEFINIDO POREM SEJA BREVE NAS DICAS)");
+		this.add(instructionTip3);
+		
+		JTextField tip = new JTextField(16);
+		tip.setBounds(100, 500, 120, 40);
+		tip.setVisible(true);
+		this.add(tip);
+		
+		Button2 submit = new Button2("Gravar Palavra", 100, 550, 200, 200);
+		submit.addActionListener(e -> this.setTwo(text.getText(), tip.getText()));
+		submit.addActionListener(e -> this.frame.setVisible("menu"));
+		this.add(submit);
+		
+		this.frame.add(this);
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	/*
+	 * Take the users input and checks if the text is an acceptable input
+	 */
+	private void setTwo(String text, String tip){
+		this.text = text;
+		this.tip = tip;
+		
+		int check = 1;
+		int finalCheck = 1;
+		
+		if(text.length() == 0 || tip.length() == 0 || text.length() > 16) {
+			JOptionPane.showMessageDialog(this.frame, "Dados invalidos, a nova palavra não foi cadastrada");
+			return;
+		}
+		
+		/*
+		 * Checks if the letters are all UpperCase or '_'
+		 */
+		for(int i = 0; i < text.length(); i++) {
+			
+			if(check == 0) {
+				finalCheck = 0;
+				break;
+			}
+			
+			check = 0;
+			
+			if(text.charAt(i) == '_') {
+				check = 1;
+			}else {
+				for(int j = 65; j < 91; j++) {
+					if(text.charAt(i) == (char) j) {
+						check = 1;
+					}
+				}	
+			}
+		}
+		
+		if(finalCheck == 1) {
+			Controller.data.newWord(this.text, this.tip, this.type);
+			JOptionPane.showMessageDialog(this.frame, "Cadastrado com sucesso");
+		} else {
+			JOptionPane.showMessageDialog(this.frame, "Dados invalidos, a nova palavra não foi cadastrada");
+		}
+		
+	}
+}
