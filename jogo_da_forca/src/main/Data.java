0bd420201133732dc0b78c542ebbbdfe7a13773f
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-04-03 20:27:12 -0300

    New function to add new words from the user

--- jogo_da_forca/src/main/Data.java
+++ jogo_da_forca/src/main/Data.java
@@ -14,9 +14,9 @@ public class Data {
 	
 	public Data() {
 		
-		Word ozzy_osbourne = new Word("OZZY_OSBOURNE","Ja mordeu um morcego","music");
-		Word kurt_cobain = new Word("KURT_COBAIN","Uma das capas de seus albums é a foto de seu filho submerso","music");
-		Word megadeth = new Word("MEGADETH","O fundador da banda foi expulso de outra ants de fundar sua propria","music");
+		Word ozzy_osbourne = new Word("OZZY_OSBOURNE","Ja mordeu um morcego","musicas");
+		Word kurt_cobain = new Word("KURT_COBAIN","Uma das capas de seus albums é a foto de seu filho submerso","musicas");
+		Word megadeth = new Word("MEGADETH","O fundador da banda foi expulso de outra ants de fundar sua propria","musicas");
 		this.music.add(ozzy_osbourne);
 		this.music.add(kurt_cobain);
 		this.music.add(megadeth);
@@ -30,10 +30,10 @@ public class Data {
 		Word batata = new Word("BATATA","É uma grande fonte de carboidratos","comidas");
 		this.foods.add(batata);
 		
-		Word abajur = new Word("ABAJUR","É um bom utensilio para ler","objects");
+		Word abajur = new Word("ABAJUR","É um bom utensilio para ler","objetos");
 		this.objects.add(abajur);
 		
-		Word golf = new Word("GOLF","Tiger Woods aprova","sports");
+		Word golf = new Word("GOLF","Tiger Woods aprova","esportes");
 		this.sports.add(golf);
 		
 		Word mona_lisa = new Word("MONA_LISA","Mais famosa impossivel","art");
@@ -47,6 +47,40 @@ public class Data {
 	public Word randomChoice(ArrayList<Word> target) {
 		int aux = (int) (Math.random() * target.size());
 		return target.get(aux);
+	}
+	
+	/*
+	 * User insert the info and create a new word
+	 */
+	public void newWord(String text, String tip, String type) {
+		if(type == "music") {
+			this.music.add(new Word(text, tip, type));
+			return;
+		}
+		if(type == "countries") {
+			this.countries.add(new Word(text, tip, type));
+			return;
+		}
+		if(type == "animals") {
+			this.animals.add(new Word(text, tip, type));
+			return;
+		}
+		if(type == "foods") {
+			this.foods.add(new Word(text, tip, type));
+			return;
+		}
+		if(type == "objects") {
+			this.objects.add(new Word(text, tip, type));
+			return;
+		}
+		if(type == "sports") {
+			this.sports.add(new Word(text, tip, type));
+			return;
+		}
+		if(type == "art") {
+			this.art.add(new Word(text, tip, type));
+			return;
+		}
 	}
 	
 }
