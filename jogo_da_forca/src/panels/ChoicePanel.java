10419f4b9bbbaa142a5f14e1051aa07a73193c31
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2021-04-13 15:13:48 -0300

    Actionlistener changed

--- jogo_da_forca/src/panels/ChoicePanel.java
+++ jogo_da_forca/src/panels/ChoicePanel.java
@@ -30,37 +30,37 @@ public class ChoicePanel extends JPanel{
 	 */
 	public void updateChoicePanel() {
 		Button2 music = new Button2("Musica", 40, 100, 200, 200);
-		music.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.music)));
+		music.addActionListener(e -> Controller.game.checkCurrentArray("music"));
 		music.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(music);
 		
 		Button2 countries = new Button2("Paises", 340, 100, 200, 200);
-		countries.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.countries)));
+		countries.addActionListener(e -> Controller.game.checkCurrentArray("countries"));
 		countries.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(countries);
 		
 		Button2 animals = new Button2("Animais", 640, 100, 200, 200);
-		animals.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.animals)));
+		animals.addActionListener(e -> Controller.game.checkCurrentArray("animals"));
 		animals.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(animals);
 		
 		Button2 foods = new Button2("Comidas", 940, 100, 200, 200);
-		foods.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.foods)));
+		foods.addActionListener(e -> Controller.game.checkCurrentArray("foods"));
 		foods.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(foods);
 		
 		Button2 objects = new Button2("Objetos", 40, 450, 200, 200);
-		objects.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.objects)));
+		objects.addActionListener(e -> Controller.game.checkCurrentArray("objects"));
 		objects.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(objects);
 		
 		Button2 sports = new Button2("Esportes", 340, 450, 200, 200);
-		sports.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.sports)));
+		sports.addActionListener(e -> Controller.game.checkCurrentArray("sports"));
 		sports.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(sports);
 		
 		Button2 art = new Button2("Arte", 640, 450, 200, 200);
-		art.addActionListener(e -> Controller.game.updateFrame(Controller.data.randomChoice(Controller.data.art)));
+		art.addActionListener(e -> Controller.game.checkCurrentArray("art"));
 		art.addActionListener(e -> this.frame.setVisible("game"));
 		this.add(art);
 		
